02366c03749bee6731b4c734fc765e29b486d214
sunilmsz <inpb91@gmail.com>
2022-05-11 10:57:28 +0530

    input validator further  reduced

--- src/controller/validator/inputValidator.js
+++ src/controller/validator/inputValidator.js
@@ -1,33 +1,13 @@
-const isValidReqBody = (obj) => {
-    if (obj == "undefined" || Object.keys(obj).length == 0)
-        return false;
-    return true
-}
+const isValidReqBody = obj => (!obj || Object.keys(obj).length == 0) ? false : true
 
-const isString = st => {
-    if (typeof st != "string" || st.trim().length === 0)
-        return false
-    return true
-}
+const isString = st => (typeof st != "string" || st.trim().length === 0) ? false : true
 
-const isOptionalString = st => {
-    if (typeof st != "undefined" && (typeof st != "string" || st.trim().length === 0))
-        return false
-    return true
-}
 
-const isNumber = num => {
-    if (typeof num != "number")
-        return false
-    return true
-}
+const isOptionalString = st => (typeof st != "undefined" && (typeof st != "string" || st.trim().length === 0)) ? false : true
 
-const isParticularString = (st, arr) =>  // string array
-{
-    if (!isString(st) || !arr.includes(st))
-        return false
-    return true
-}
+const isNumber = num => (typeof num != "number") ? false : true
+
+const isParticularString = (st, arr) => (!isString(st) || !arr.includes(st)) ? false : true
 
 
 // to check the values present in obj are coming or not,should be string, 
@@ -39,19 +19,19 @@ const allString = (obj, optional) => {
             if (!obj[key]) error += key + " "
             if (!isString(obj[key])) error2 += key + " "
         }
-        if (optional == true && isOptionalString(obj[key])) 
-                error2 += key + " "
+        if (optional == true && isOptionalString(obj[key]))
+            error2 += key + " "
     }
-    return error.length != 0 ? [false, "You'r missing Mandatory fields :- " + error] : 
-     (error2.length != 0)? [false, error2 + " should have valid string datatype and should be non-empty"]
-    :[true]
+    return error.length != 0 ? [false, "You'r missing Mandatory fields :- " + error] :
+        (error2.length != 0) ? [false, error2 + " should have valid string datatype and should be non-empty"]
+            : [true]
 }
 // here obj can be contatin one or more array
 // we are cheking the elements of array should have string datatype
 // and should not contain empty strings
 const arrHasString = (obj) => {
     for (let key in obj) {
-        if (typeof obj[key] == 'undefined' || !Array.isArray(obj[key]))
+        if (!obj[key] || !Array.isArray(obj[key]))
             return [false, key + " is a mandatory field and should be an array"]
         for (let i = 0; i < obj[key].length; i++) {
             if (typeof (obj[key][i]) !== "string")
